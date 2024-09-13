edc5439f65223b659b85a325efac654038c57b1d
YangJJune <58018839+YangJJune@users.noreply.github.com>
2024-09-13 11:15:10 +0900

    style: 로그인 화면 중앙 및 폰트 추가

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -48,7 +48,7 @@ const LoginPage = () => {
 	};
 	const onClickHandler = () => {};
 	return (
-		<div style={{ display: 'flex', justifyContent: 'center', height: '100%' }}>
+		<div style={{ display: 'flex', justifyContent: 'center', height: '100vh' }}>
 			<div className="LoginDiv" style={{ width: '580px' }}>
 				<div className="LoginTitleDiv">유니페스 총학생회 관리페이지</div>
 				<div className="LoginBoldDiv">로그인</div>
